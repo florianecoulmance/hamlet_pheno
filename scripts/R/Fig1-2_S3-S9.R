67cdb71981f3309bbd608710f0a2ab45007c3da6
Floriane Coulmance <fcoulman@SORGE44448>
2025-10-28 13:12:41 +0100

    pheno r code

--- scripts/R/Fig1-2_S3-S9.R
+++ scripts/R/Fig1-2_S3-S9.R
@@ -202,6 +202,7 @@ pca_plot <- function(pca_data, pc_first, pc_second, species_info, geo_info, var,
       guides(color = guide_legend(nrow = legend_rows))
     
     legend <- cowplot::get_legend(dummy_plot)
+    print("The dummy plot is: ")
     print(dummy_plot)
     return(legend)
   }
