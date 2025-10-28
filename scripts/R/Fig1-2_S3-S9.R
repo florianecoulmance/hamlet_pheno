cf8503eb9d995544072b1c2366c154d30cc37c26
Floriane Coulmance <fcoulman@SORGE44448>
2025-10-28 09:47:41 +0100

    pheno r code

--- scripts/R/Fig1-2_S3-S9.R
+++ scripts/R/Fig1-2_S3-S9.R
@@ -216,7 +216,7 @@ pca_plot <- function(pca_data, pc_first, pc_second, species_info, geo_info, var,
     p <- p +
       geom_image(
         data = centroids,
-        aes(image = link),
+        aes(x = x, y = y, image = link),
         vjust = 1, hjust = 0, size = 0.1, asp = 1.1, alpha = 1
       )
   }
