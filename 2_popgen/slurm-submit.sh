cedd9151693ba56f682f985483d776f8d16327f4
Floriane Coulmance <fcoulman@SORGE44448>
2025-11-04 09:38:15 +0100

    slurm sh

---
+++ 2_popgen/slurm-submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=smk_{rule}
+#SBATCH --output=/fs/dss/work/doau0129/hamlet_pheno/logs/{rule}.%j.out
+#SBATCH --error=/fs/dss/work/doau0129/hamlet_pheno/logs/{rule}.%j.err
+#SBATCH --time={resources.time}
+#SBATCH --mem={resources.mem_mb}
+#SBATCH --cpus-per-task={threads}
+#SBATCH --partition=rosa.p
+
+
+# Execute job
+{exec_job}
