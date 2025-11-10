a1cdeddda808528bfcfa9b459dbe2888369ef6f9
Floriane Coulmance <fcoulman@SORGE44448>
2025-11-10 15:21:03 +0100

    plots

--- scripts/R/helper_functions.R
+++ scripts/R/helper_functions.R
@@ -477,8 +477,6 @@ hierClustering <- function(data_path, pca_file, species_col, geo_map, color_by =
   # -----------------------------
   # 5. Plot tree
   # -----------------------------
-  tree$height <- tree$height / max(tree$height)
-
   t <- ggtree(tree, layout = "fan", ladderize = FALSE, size = 0.5, color = scol) +
     geom_tippoint(aes(color = .data[[group_col]]), size = 3, alpha = 0.5) +
     scale_color_manual(values = color_map, name = legend_name) +
@@ -486,8 +484,7 @@ hierClustering <- function(data_path, pca_file, species_col, geo_map, color_by =
       legend.position = ifelse(extract_legend, "right", "none"),
       plot.title = element_text(size = 14, color = "gray20", face = "bold"),
       plot.subtitle = element_text(size = 10, color = "gray20")
-    ) +
-    coord_equal()
+    )
   
   # -----------------------------
   # 6. Get title
