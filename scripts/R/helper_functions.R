3e0a2be1b67f577f356f7e6ff694fc5b97fdbb44
Floriane Coulmance <fcoulman@SORGE44448>
2025-11-12 18:46:30 +0100

    plots

--- scripts/R/helper_functions.R
+++ scripts/R/helper_functions.R
@@ -155,7 +155,7 @@ legend_plot <- function(info_table) {
   geom_point(aes(x = col - 0.2, y = y_dot, color = spec), size = 10, show.legend = FALSE) +
   scale_color_manual(values = color_map) +
   # logo
-  geom_image(aes(x = col, y = y_logo, image = link), size = 0.8, asp = 1.1) +
+  geom_image(aes(x = col, y = y_logo, image = link), size = 0.7, asp = 1.1) +
   # species name
   geom_text(aes(x = col + 0.01, y = y_text, label = paste0("H. ", Species)), size = 4, vjust = 1, fontface = "italic") +
   theme_void() +
