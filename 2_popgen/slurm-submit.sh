c703aca3e3f8d0924be50a3cee6d9723d2c81e8d
Floriane Coulmance <fcoulman@SORGE44448>
2026-04-24 18:15:06 +0200

    newhybrid

--- 2_popgen/slurm-submit.sh
+++ 2_popgen/slurm-submit.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=smk_{rule}
 #SBATCH --output=/fs/dss/work/doau0129/hamlet_pheno/logs/{rule}.%j.out
 #SBATCH --error=/fs/dss/work/doau0129/hamlet_pheno/logs/{rule}.%j.err
-#SBATCH --runtime={resources.time}
+#SBATCH --runtime={resources.runtime}
 #SBATCH --mem={resources.mem_mb}
 #SBATCH --cpus-per-task={threads}
-#SBATCH --partition=rosa.p
-#SBATCH --account=agfisheco
+#SBATCH --slurm_partition=rosa.p
+#SBATCH --slurm_account=agfisheco
 
 # Execute job
 {exec_job}
